e33fa5af2dc9d860078846988443c5dcfd4b18f4
carloshpdoc <carloshenrique.pdoc>
2017-06-21 13:07:02 -0300

    first commit

---
+++ tests/TextTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CodeBot\Message;
+
+use PHPUnit\Framework\TestCase;
+
+class TextTest extends TestCase
+{
+		public function testRetornaUmArray ()
+		{
+			$actual = (new Text(recipient 1))->message(messageText'oiii');
+			$expected = [
+				'recipient' =>[
+					'id'=>1
+				],
+				"message"=>[
+					'text'=>'oiii',
+					'metadada'=>'DEVELOPER_DEFINED_METADA'
+				]				
+			];
+			
+			$this->assertEquals($actual,$expected);
+		}
+}
